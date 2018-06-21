97044b7765065fe776f008d37f7b1e783d028676
Tống Thiện Minh <minh.tt@teko.vn>
2018-06-21 18:21:54 +0700

    Create SearchEngine

--- app/src/main/java/com/manminh/simplechem/balance/engine/BalanceEngine.java
+++ app/src/main/java/com/manminh/simplechem/balance/engine/BalanceEngine.java
@@ -2,14 +2,7 @@ package com.manminh.simplechem.balance.engine;
 
 import com.manminh.simplechem.model.Equation;
 
-<<<<<<< Updated upstream
-=======
-import java.util.ArrayList;
 
-/**
- * Interface of balance algorithm implementation
- */
->>>>>>> Stashed changes
 public interface BalanceEngine {
     void balance(Equation equation);
 }
