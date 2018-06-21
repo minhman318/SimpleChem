19892abf3387075b4898490c778e749274f05756
Nguyen Minh Man <minhman133@outlook.com>
2018-06-21 21:47:06 +0700

    Commit exception package

---
+++ app/src/main/java/com/manminh/simplechem/balance/exception/ParseEquationException.java
@@ -0,0 +1,24 @@
+package com.manminh.simplechem.balance.exception;
+
+public class ParseEquationException extends Exception {
+    private static String EXCEPTION_MSG = "Parse equation exception";
+
+    public static final int INVALID_SYNTAX = 0;
+    public static final int INVALID_EQUATION = 1;
+
+    private int mExceptionCode = -1;
+
+    public ParseEquationException(int code) {
+        super(EXCEPTION_MSG + ". Code: " + String.valueOf(code));
+        mExceptionCode = code;
+    }
+
+    public ParseEquationException(int code, Throwable cause) {
+        super(EXCEPTION_MSG + ". Code: " + String.valueOf(code), cause);
+        mExceptionCode = code;
+    }
+
+    public int getExceptionCode() {
+        return mExceptionCode;
+    }
+}
